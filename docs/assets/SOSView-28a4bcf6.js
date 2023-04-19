bc59fed12a05ea55e4493481a09b59b1617e3742
Martyna-Rymer <martyna.rymer@appliscale.io>
2023-04-19 17:53:07 +0200

    further changes

--- docs/assets/SOSView-28a4bcf6.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,o as r,c as s}from"./index-ebe6558c.js";const i={methods:{vibrateSOS(){console.log("vibrate");const o=100,e=300,t=200,n=[o,t,o,t,o,t,e,t,e,t,e,t,o,t,o,t,o];navigator.vibrate(n)}},mounted(){this.vibrateSOS()}};function c(o,e,t,n,l,u){return r(),s("h1",null,"SOS")}const S=a(i,[["render",c]]);export{S as default};
